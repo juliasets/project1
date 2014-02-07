468bc1561810d63cf57e2ee46248c01fd5c53811
elizabethkilson <elizabethkilson@gmail.com>
2014-02-07 15:59:08 -0500

    Adding Output

---
+++ Output.java
@@ -0,0 +1,24 @@
+package project1;
+
+public class Output{
+	private int[][] data;
+	
+	private boolean[][] set;
+	
+	private double xmin;
+	private double xmax;
+	private double ymin;
+	private double ymax;
+	private double resolution;
+	
+	public output(Job j)
+	{
+	
+	}
+	
+	
+	public insertData(Job j, int[][] data)
+	{
+	
+	}
+}
