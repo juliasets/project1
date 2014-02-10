d4dc57c806f193abbf984fafa664c0d87e81888c
Nick <nick@ubuntu.ubuntu-domain>
2014-02-10 17:45:50 -0500

    Generation of consumers checks for the number of cores and adds based on that. One core per consumer.

--- src/main/java/project1/ProducerConsumerTest.java
+++ src/main/java/project1/ProducerConsumerTest.java
@@ -3,35 +3,39 @@ package project1;
 import org.eclipse.swt.widgets.*;
 
 public class ProducerConsumerTest {
-
-	public void run() {
-		Display display = new Display();
-		Shell shell = new Shell(display);
-		shell.setText("Julia");
-		DisplayPixels dp = new DisplayPixels(shell);
-
-
+    
+    public void run() {
+	Display display = new Display();
+	Shell shell = new Shell(display);
+	shell.setText("Julia");
+	DisplayPixels dp = new DisplayPixels(shell);
+	
+	
         Drop d = new Drop();
         Producer p1 = new Producer(d, dp);
-        Consumer c1 = new Consumer(d, 1);
-        Consumer c2 = new Consumer(d, 2);
-        Consumer c3 = new Consumer(d, 3);
+	int cores = Runtime.getRuntime().availableProcessors();
+	ArrayList<Consumer> consumers;
+	for (int i = 1; i <= cores; ++i)
+	    {
+		Consumer c = new Consumer(d, i);
+		consumers.add(c);
+	    }
         p1.start();
-        c1.start();
-        c2.start();
-        c3.start();
-
-		shell.open();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
-
-		display.dispose();
+	for (int i = 0; i < cores; ++i)
+	    {
+		consumers.get(i).start();
+	    }
+	shell.open();
+	while (!shell.isDisposed()) {
+	    if (!display.readAndDispatch()) {
+		display.sleep();
+	    }
 	}
-
-	public static void main(String[] args) {
-		new ProducerConsumerTest().run();
+	
+	display.dispose();
+    }
+    
+    public static void main(String[] args) {
+	new ProducerConsumerTest().run();
     }
 }
